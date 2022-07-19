c406f4ec9cfe79ca12f2dfcec33b6c7da8ecc3b9
ngud-119 <passiondev7@gmail.com>
2022-07-19 23:55:24 +0530

    Initial Commit

---
+++ Get Fit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Get Fit
+//
+//  Created by Sandeep Sahani on 19/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
