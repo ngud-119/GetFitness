72d60764a0cd6e5499b04bac5b33bd277e16b779
ngud-119 <passiondev7@gmail.com>
2022-10-02 23:10:30 +0530

    Add and modify file

--- Get Fit/Controllers/CardCell.swift
+++ Get Fit/Controllers/CardCell.swift
@@ -7,17 +7,35 @@
 
 import UIKit
 
-class CardCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+class CardCell: UITableViewCell
+{
+    @IBOutlet weak var cardView: UIView!
+    @IBOutlet weak var cardBackgroundImageView: UIImageView!
+    @IBOutlet weak var workoutLevelLabel: UILabel!
+    @IBOutlet weak var workoutNameLabel: UILabel!
+    @IBOutlet weak var equipmentTypeLabel: UILabel!
+    @IBOutlet weak var workoutTypeLabel: UILabel!
+    @IBOutlet weak var totalWorkoutsLabel: UILabel!
+    @IBOutlet weak var totalTimeLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
+    
+    // Function for setting up the cell.
+    func configure(image: UIImage,workoutLevel: String,workoutName: String,equipmentType: String,workoutType: String,totalWorkouts:Int,totalTime: Int)
+    {
+        cardBackgroundImageView.image = image
+        workoutLevelLabel.text = workoutLevel
+        workoutNameLabel.text = workoutName
+        equipmentTypeLabel.text = equipmentType
+        workoutTypeLabel.text = workoutType
+        totalWorkoutsLabel.text = "Total workouts: \(totalWorkouts)"
+        totalTimeLabel.text = "Total time: \(totalTime) minutes"
+        
+        cardView.layer.cornerRadius = 10.0 // Make cell rounded
+        cardView.layer.borderWidth = 0.5 // Set a line along the border
+        cardView.layer.borderColor = UIColor.gray.cgColor // Set the color of border line
+        
+        cardBackgroundImageView.layer.cornerRadius = 10.0 // Setting card image corner radius
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+    
+    
 }
--- Get Fit/Controllers/WorkoutsViewController.swift
+++ Get Fit/Controllers/WorkoutsViewController.swift
@@ -5,4 +5,40 @@
 //  Created by Sandeep Sahani on 02/10/22.
 //
 
-import Foundation
+import UIKit
+
+// Conformimg to the UITableViewDelegate and UITableViewDataSource protols to perform important operation with table view.
+class WorkoutsViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
+{
+    
+    @IBOutlet weak var workoutsCardTableView: UITableView!
+    
+    let workoutLevel: [String] = ["BEGINNER LEVEL","MODRATE LEVEL","ADVANCE LEVEL"] // Array for storing the workout level text.
+    let workoutName: [String] = ["Strong and Big Chest","Back Workout","Bicep Workout"] // Array for stroing the workout name.
+    let equiptmentType: [String] = ["Full Equipment","No Equipment","Body Weight"] // Array for strting the equiptment label.
+    let workoutType: [String] = ["Strength","Endurance","Weight"] // Array for storing the workout name.
+    let totalWorkouts: [Int] = [6,7,5] // Array for storing total workouts.
+    let totalTime: [Int] = [45,55,60] // Array for storing total time of workout.
+    let cardImages: [UIImage] = [UIImage(named: "Chest.jpeg")!,UIImage(named: "Back.jpeg")!,UIImage(named: "Biceps.jpeg")!] // Array for storing the card images.
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+    
+    // For creating desired rows in the table view.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return cardImages.count // Return total numbers of images
+    }
+    
+    // Returns what cell are being used
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        // Use to populate different parameters of custom cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cardCell", for: indexPath) as! CardCell
+        
+        cell.configure(image: cardImages[indexPath.row], workoutLevel: workoutLevel[indexPath.row], workoutName: workoutName[indexPath.row], equipmentType: equiptmentType[indexPath.row], workoutType: workoutType[indexPath.row], totalWorkouts: totalWorkouts[indexPath.row], totalTime: totalTime[indexPath.row])
+        
+        return cell
+    }
+}
