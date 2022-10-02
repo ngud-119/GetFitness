48eb44c777a5dfaf94374372b747235f26246927
ngud-119 <passiondev7@gmail.com>
2022-10-02 23:08:59 +0530

    Modify file

---
+++ Get Fit/Controllers/CardCell.swift
@@ -0,0 +1,23 @@
+//
+//  CardCell.swift
+//  Get Fit
+//
+//  Created by Sandeep Sahani on 02/10/22.
+//
+
+import UIKit
+
+class CardCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Get Fit/Controllers/WorkoutsViewController.swift
@@ -0,0 +1,8 @@
+//
+//  WorkoutsViewController.swift
+//  Get Fit
+//
+//  Created by Sandeep Sahani on 02/10/22.
+//
+
+import Foundation
