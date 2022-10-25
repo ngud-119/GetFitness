f68b455006dd439ce19300edd346b00cb1aa42b4
ngud-119 <passiondev7@gmail.com>
2022-10-25 19:38:07 +0530

    Conform to UITextFieldDelegate to dismiss the keyboard after pressing return

--- Get Fit/Controllers/Authentication/SignInViewController.swift
+++ Get Fit/Controllers/Authentication/SignInViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class SignInViewController: UIViewController,UITextFieldDelegate
+class SignInViewController: UIViewController
 {
     @IBOutlet weak var fitnessText: UILabel!
     @IBOutlet weak var fingertipsText: UILabel!
@@ -15,29 +15,35 @@ class SignInViewController: UIViewController,UITextFieldDelegate
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var SignInButton: UIButton!
-
+    @IBOutlet weak var errorLabel: UILabel!
     override func viewDidLoad()
     {
         super.viewDidLoad()
         self.emailTextField.delegate = self
         self.passwordTextField.delegate = self
+        errorLabel.alpha = 0 // Hiding erorr label
     }
     
     @IBAction func signInButtonTapped(_ sender: Any)
     {
-        var text: String? = emailTextField.text
-        var pass: String? = passwordTextField.text
+        let text: String? = emailTextField.text
+        let pass: String? = passwordTextField.text
         print("email is "+text!)
         print("Password is "+pass!)
         
     }
-    
+
+}
+
+// MARK: Extension for dismissing keyboard when return is tapped.
+
+extension SignInViewController: UITextFieldDelegate
+{
     func textFieldShouldReturn(_ textField: UITextField) -> Bool
     {
         self.view.endEditing(true)
         return false
     }
-    
 }
 
 
--- Get Fit/Controllers/Authentication/SignUpViewController.swift
+++ Get Fit/Controllers/Authentication/SignUpViewController.swift
@@ -9,9 +9,37 @@ import UIKit
 
 class SignUpViewController: UIViewController
 {
+    @IBOutlet weak var userName: UITextField!
+    @IBOutlet weak var userMobileNumber: UITextField!
+    @IBOutlet weak var userEmail: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var errorLabel: UILabel!
+    @IBOutlet weak var signupButton: UIButton!
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        self.userName.delegate = self
+        self.userMobileNumber.delegate = self
+        self.userEmail.delegate = self
+        self.password.delegate = self
+        errorLabel.alpha = 0 // Hide error label
         
+    }
+    
+    @IBAction func signupButtonTapped(_ sender: UIButton)
+    {
+        
+    }
+}
+
+// MARK: Extension for dismissing keyboard when return is pressed.
+
+extension SignUpViewController: UITextFieldDelegate
+{
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool
+    {
+        self.view.endEditing(true)
+        return false
     }
 }
