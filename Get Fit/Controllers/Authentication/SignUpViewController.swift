2fd63845566141e717b097f9c8fb97cc38a58a23
ngud-119 <passiondev7@gmail.com>
2022-10-25 23:51:12 +0530

    Added code for validating fields

--- Get Fit/Controllers/Authentication/SignUpViewController.swift
+++ Get Fit/Controllers/Authentication/SignUpViewController.swift
@@ -27,9 +27,55 @@ class SignUpViewController: UIViewController
         
     }
     
+    // Check the fields and validate that the data are correct. If everything is correct this methopd returns nil. Otherwise, it returns error message.
+    func validateField() -> String?
+    {
+        // Check that all fields are filled in.
+        if userName.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" || userMobileNumber.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" || userEmail.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" || password.text?.trimmingCharacters(in: .whitespacesAndNewlines) == ""
+        {
+            return "Please fill in all fields."
+        }
+        
+        // Check if the password is secure.
+        
+        let cleanedPassword = password.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        if Utilities.isPasswordValid(cleanedPassword) == false
+        {
+            // Password is not secure enough.
+            
+            return "Please make sure your password is atleast 8 characters, contains a special character and a number."
+        }
+    
+       return nil
+    }
+    
+    // Method for showing error
+    func showError(_ message: String)
+    {
+        errorLabel.text = message
+        errorLabel.alpha = 1
+    }
+    
     @IBAction func signupButtonTapped(_ sender: UIButton)
     {
+        // Validate the fields
+        let error = validateField()
         
+        if error != nil
+        {
+            // There's something wrong with fields.
+            showError(error!)
+        }
+        
+        // if all the fields are correct then
+        else
+        {
+            // Create the user
+            
+            // Transtion to the user
+        }
+    
     }
 }
 
---
+++ Get Fit/Utilities/Utilities.swift
@@ -0,0 +1,8 @@
+//
+//  Utilities.swift
+//  Get Fit
+//
+//  Created by Sandeep Sahani on 25/10/22.
+//
+
+import Foundation
