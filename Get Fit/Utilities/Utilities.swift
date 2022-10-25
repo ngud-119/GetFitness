72e8e0c7ac35dd164fe4ffa816d37f848ba9a46d
ngud-119 <passiondev7@gmail.com>
2022-10-25 23:55:58 +0530

    Added method to check for valid password

--- Get Fit/Utilities/Utilities.swift
+++ Get Fit/Utilities/Utilities.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+// Struct for utiliy methods
+struct Utilities
+{
+    // Utility function for checking for a valid and strong passord
+    static func isPasswordValid(_ password : String) -> Bool
+    {
+        let passwordTest = NSPredicate(format: "SELF MATCHES %@", "^(?=.*[a-z])(?=.*[$@$#!%*?&])[A-Za-z\\d$@$#!%*?&]{8,}")
+        return passwordTest.evaluate(with: password)
+    }
+}
