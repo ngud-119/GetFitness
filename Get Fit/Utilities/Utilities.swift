967c80112201fa8ded4969628062af1d8e98949e
ngud-119 <passiondev7@gmail.com>
2022-10-28 11:54:17 +0530

    Added function to check valid email

--- Get Fit/Utilities/Utilities.swift
+++ Get Fit/Utilities/Utilities.swift
@@ -15,4 +15,10 @@ struct Utilities
         let passwordTest = NSPredicate(format: "SELF MATCHES %@", "^(?=.*[a-z])(?=.*[$@$#!%*?&])[A-Za-z\\d$@$#!%*?&]{8,}")
         return passwordTest.evaluate(with: password)
     }
+    // Utility function for checking for a valid email
+    static func isValidEmail(_ email: String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailPred.evaluate(with: email)
+    }
 }
