6098eb7dc379fc2f99ffea4028edd8be2ddddb37
ngud-119 <passiondev7@gmail.com>
2022-10-30 23:13:35 +0530

    Modified

--- Get Fit/AppDelegate.swift
+++ Get Fit/AppDelegate.swift
@@ -9,9 +9,8 @@ import UIKit
 import FirebaseCore
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+class AppDelegate: UIResponder, UIApplicationDelegate
+{
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool
     {
