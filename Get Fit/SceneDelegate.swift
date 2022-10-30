36a637c3c70c6ab29da0391cd906a33b01b1fed7
ngud-119 <passiondev7@gmail.com>
2022-10-30 22:57:44 +0530

    Added code for transitioning when user logged in successfully.

--- Get Fit/SceneDelegate.swift
+++ Get Fit/SceneDelegate.swift
@@ -6,18 +6,48 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
+    func changeRootViewController(_ vc: UIViewController, animated: Bool = true)
+    {
+        guard let window = self.window else
+        {
+            return
+        }
+        
+        // change the root view controller to your specific view controller
+        window.rootViewController = vc
+    }
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions)
     {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        
+        let mainStoryboard = UIStoryboard(name: Storyboards.Name.Main, bundle: nil)
+        let tabBarStoryboard = UIStoryboard(name: Storyboards.Name.TabBar, bundle: nil)
+        
+        // If user is logged in before.
+        if let loggedUserName = Auth.auth().currentUser?.displayName
+        {
+            // instantiate the tab bar controller and set it as root view controller
+            let tabBarController = tabBarStoryboard.instantiateViewController(withIdentifier: Storyboards.VCID.TabBarController)
+            window?.rootViewController = tabBarController
+        }
+        
+        else
+        {
+            // if user isn't logged in
+            // instantiate the navigation controller and set it as root view controller
+            let signInNavController = mainStoryboard.instantiateViewController(withIdentifier: Storyboards.VCID.SignInNavigationController)
+            window?.rootViewController = signInNavController
+        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene)
