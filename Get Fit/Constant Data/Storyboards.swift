8bf022f978126b43da97676075374dcc6ae3f320
ngud-119 <passiondev7@gmail.com>
2022-10-30 23:02:30 +0530

    Added constants for storyboard names and IDs

--- Get Fit/Constant Data/Storyboards.swift
+++ Get Fit/Constant Data/Storyboards.swift
@@ -7,9 +7,20 @@
 
 struct Storyboards
 {
+    struct Name
+    {
+        static let Main = "Main"
+        static let TabBar = "TabBar"
+    }
+    struct VCID
+    {
+        static let SignInNavigationController = "SignInNavigationController"
+        static let TabBarController = "TabBarController"
+    }
     struct segues
     {
         static let SignupVCToTabBarView = "signupVCToTabBar"
         static let SigninVCToTabBarView = "signinVCToTabBar"
+        static let SignInVC = "SignInVC"
     }
 }
