25b977be1fddca33a75c76cc29638675f78d0634
ngud-119 <passiondev7@gmail.com>
2022-11-02 22:06:41 +0530

    Fetch data

--- Get Fit/Utilities/Utilities.swift
+++ Get Fit/Utilities/Utilities.swift
@@ -5,9 +5,11 @@
 //  Created by Sandeep Sahani on 25/10/22.
 //
 
-import Foundation
-// Struct for utiliy methods
-struct Utilities
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseAuth
+// Class for utiliy methods
+class Utilities
 {
     // Utility function for checking for a valid and strong passord
     static func isPasswordValid(_ password : String) -> Bool
@@ -20,5 +22,65 @@ struct Utilities
         let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
         let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
         return emailPred.evaluate(with: email)
+    }
+    
+    static func fetchUserData(userUid: String) -> UserData?
+    {
+        var userName:String = ""
+        var mobileNumber: String = ""
+        var email: String = ""
+        let currentUserUid = Auth.auth().currentUser?.uid
+        
+        let db = Firestore.firestore()
+        
+        //        db.collection("users").document(currentUserUid!).getDocument { Snapshot, error in
+        //
+        //            // Check for errors
+        //            if let error = error
+        //            {
+        //                print("Error getting document \(error)")
+        //            }
+        //
+        //            else
+        //            {
+        //                let username = Snapshot?.get("username")
+        //                print(username)
+        //            }
+        //
+        //        }
+        db.collection("users").getDocuments { (querySnapshot,error) in
+            
+            // Check for errors
+            if let error = error
+            {
+                print("Error getting document \(error)")
+            }
+            
+            else
+            {
+                
+                for document in querySnapshot!.documents
+                {
+                    let data = document.data()
+                    let uids = document.documentID
+                    if let userName = data["username"] as? String,let mobileNumber = data["mobile number"] as? String,let email = data["userEmail"] as? String
+                    {
+//                        userName = document.get("username") as! String
+//                        mobileNumber = document.get("mobile number") as! String
+//                        email = document.get("userEmail") as! String
+                        print("Doc id :\(uids)")
+                        print("User name :\(userName)")
+                        print("Mobile number: \(mobileNumber)")
+                        print("Email: \(email)")
+                    }
+                    
+                }
+                
+            }
+            
+            
+        }
+        
+        return nil
     }
 }
