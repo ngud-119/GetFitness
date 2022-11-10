015f19d6c31a108317452c8712716927cc9512a2
ngud-119 <passiondev7@gmail.com>
2022-11-10 11:33:12 +0530

    Added functionality to pick image from Photos

--- Get Fit/Controllers/Profile Screen/ProfileViewController.swift
+++ Get Fit/Controllers/Profile Screen/ProfileViewController.swift
@@ -80,6 +80,11 @@ class ProfileViewController: UIViewController
         (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.changeRootViewController(signInSignUpViewController)
     }
     
+    @IBAction func editButtonTapped(_ sender: UIButton)
+    {
+       selectProfileImage()
+    }
+    
     @IBAction func signOutButtonTapped(_ sender: UIButton)
     {
         // Code for sign out user.
@@ -96,5 +101,47 @@ class ProfileViewController: UIViewController
             print("Error signing out: %@", signOutError)
         }
         
+   }
+    
+}
+
+// MARK: Extension for picking image from Photo's
+
+extension ProfileViewController: UIImagePickerControllerDelegate,UINavigationControllerDelegate
+{
+    private func selectProfileImage()
+    {
+        let picker = UIImagePickerController()
+        
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any])
+    {
+        var selectedImageFromPicker: UIImage?
+        
+        if let editedImage = info[UIImagePickerController.InfoKey(rawValue: "UIImagePickerControllerEditedImage")] as? UIImage
+        {
+            selectedImageFromPicker = editedImage
+        }
+        
+        else if let originalImage = info[UIImagePickerController.InfoKey(rawValue: "UIImagePickerControllerOrignalImage")] as? UIImage
+        {
+            selectedImageFromPicker = originalImage
+        }
+        
+        if let selectedImage = selectedImageFromPicker
+        {
+            userImage.image = selectedImage
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController)
+    {
+        dismiss(animated: true, completion: nil)
     }
 }
