a3bc0103b8154924c9e19d20d4d36de020086e86
ngud-119 <passiondev7@gmail.com>
2022-11-20 23:28:01 +0530

    Changes made

--- Get Fit/Constant Data/ExerciseList.swift
+++ Get Fit/Constant Data/ExerciseList.swift
@@ -11,5 +11,5 @@ struct ExerciseList
     public static let cellImage: [UIImage] = [UIImage(named: "Bench Press")!,UIImage(named: "Dumbbell Press")!,UIImage(named: "Incline Dumbbell Press")!,UIImage(named: "Decline Dumbbell Press")!,UIImage(named: "Dumbbell Flyes")!]
     public static let exerciseName: [String] = ["Bench Press","Dumbbell Press","Incline Dumbbell Press","Decline Dumbbell Press","Dumbbell Flyes"]
     public static let totalSets: [Int] = [2,3,4,5,6]
-    public static let repRange: [String] = ["10-12","10-12","10-12","10-12","10-12"]
+    public static let repRange: [String] = ["6-8","8-10","2-4","2-6","5-10"]
 }
