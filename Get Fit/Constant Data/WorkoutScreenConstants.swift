0a616afde5c6912cc351d59aa42efc0647d6dc9a
ngud-119 <passiondev7@gmail.com>
2022-11-23 23:09:57 +0530

    Modified constant data

--- Get Fit/Constant Data/WorkoutScreenConstants.swift
+++ Get Fit/Constant Data/WorkoutScreenConstants.swift
@@ -10,11 +10,12 @@ import UIKit
 // Struct tot store constants string used is workout screen
 struct WorkoutScreenConstants
 {
-    public static let workoutLevel: [String] = ["BEGINNER LEVEL","MODRATE LEVEL","ADVANCE LEVEL"] // Array for storing the workout level text.
-    public static let workoutName: [String] = ["Chest Workout","Back Workout","Bicep Workout"] // Array for stroing the workout name.
-    public static let equiptmentType: [String] = ["Full Equipment","No Equipment","Body Weight"] // Array for strting the equiptment label.
-    public static let workoutType: [String] = ["Strength","Endurance","Weight"] // Array for storing the workout name.
-    public static let totalWorkouts: [Int] = [6,7,5] // Array for storing total workouts.
-    public static let totalTime: [Int] = [45,55,60] // Array for storing total time of workout.
-    public static let cardImages: [UIImage] = [UIImage(named: "Chest.jpg")!,UIImage(named: "Back.jpeg")!,UIImage(named: "Biceps.jpeg")!] // Array for storing the card images.
+    // Array for storing the card images.
+    public static let cardImages: [UIImage] = [UIImage(named: "Cardio")!,UIImage(named: "Shoulder")!,UIImage(named: "Biceps.jpeg")!,UIImage(named: "Chest.jpg")!,UIImage(named: "Back.jpeg")!,UIImage(named: "Leg.jpeg")!]
+    
+    // Array for stroing the workout name.
+    public static let workoutName: [String] = ["Cardio","Shoulder Workout","Bicep Workout","Chest Workout","Back Workout","Leg Workout"]
+    
+    // Array for storing total workouts.
+    public static let totalWorkouts: [Int] = [6,7,5,12,22,33]
 }
