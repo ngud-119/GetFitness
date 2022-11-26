1cf3a8459e33b69b720ce21dad50f312e47761f4
ngud-119 <passiondev7@gmail.com>
2022-11-26 23:56:34 +0530

    Added class to fetch data from exercise list view controller from API

---
+++ Get Fit/Utilities/ExerciseListData.swift
@@ -0,0 +1,32 @@
+//
+//  ExerciseList.swift
+//  Get Fit
+//
+//  Created by Sandeep Sahani on 26/11/22.
+//
+
+// Class for getting data from API for exercise list view controller.
+import UIKit
+
+class ExerciseListData
+{
+    static func getExerciseWithBodyPart(bodyPart: String)
+    {
+        // URL
+        
+        // URL Request
+        
+        // Specify the header
+        
+        // Specify the body
+        
+        // Set the request type
+        
+        // Get the URLSession
+        
+        // Create data task
+        
+        // Fire off the data
+    }
+}
+
