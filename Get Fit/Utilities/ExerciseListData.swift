cca893310e12fda5cd426675cd86eb6ce381c175
ngud-119 <passiondev7@gmail.com>
2022-11-27 13:18:14 +0530

    Class for fetching and parsing JSON data based on #3

--- Get Fit/Utilities/ExerciseListData.swift
+++ Get Fit/Utilities/ExerciseListData.swift
@@ -13,20 +13,58 @@ class ExerciseListData
     static func getExerciseWithBodyPart(bodyPart: String)
     {
         // URL
+        let url = URL(string: "https://exercisedb.p.rapidapi.com/exercises/bodyPart/\(bodyPart)")
+        
+        guard url != nil else
+        {
+            print("Error creating url object!")
+            return
+        }
         
         // URL Request
+        var request = URLRequest(url: url!, cachePolicy: .useProtocolCachePolicy, timeoutInterval: 10)
         
         // Specify the header
+        let headers = [
+            "X-RapidAPI-Key": "541dd37637msh5d41896e86719efp19da0ejsn8c426b6b0b58",
+            "X-RapidAPI-Host": "exercisedb.p.rapidapi.com"
+        ]
         
+        request.allHTTPHeaderFields = headers
+
         // Specify the body
         
         // Set the request type
+        request.httpMethod = "GET"
         
         // Get the URLSession
+        let session = URLSession.shared
         
         // Create data task
+        let dataTask = session.dataTask(with: request) { data, response, error in
+            
+            // Check for errors
+            if error == nil && data != nil
+            {
+                let decoder = JSONDecoder()
+                // Try to parse JSON data
+                if let data = data
+                {
+                    do
+                    {
+                        let exerciseList = try decoder.decode([ExerciseDescription].self, from: data)
+                        print(exerciseList)
+                    }
+                    catch
+                    {
+                        print(error.localizedDescription)
+                    }
+                }
+            }
+        }
         
         // Fire off the data
+        dataTask.resume()
     }
 }
 
