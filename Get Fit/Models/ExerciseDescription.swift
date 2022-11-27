05ff7c6f661bcc4501568388cf40cf0bf2f61813
ngud-119 <passiondev7@gmail.com>
2022-11-27 13:14:34 +0530

    Struct for decodingAPIs JSON

---
+++ Get Fit/Models/ExerciseDescription.swift
@@ -0,0 +1,19 @@
+//
+//  ExerciseDescription.swift
+//  Get Fit
+//
+//  Created by Sandeep Sahani on 27/11/22.
+//
+
+// Exercise model for JSON
+import Foundation
+
+struct ExerciseDescription: Codable
+{
+    var bodyPart: String
+    var equipment: String
+    var gifUrl: String
+    var id: String
+    var name: String
+    var target: String
+}
