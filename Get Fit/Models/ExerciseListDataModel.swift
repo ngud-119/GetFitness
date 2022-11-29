5d8d986f97e732d259bea0c7acab373d9b8f113c
ngud-119 <passiondev7@gmail.com>
2022-11-29 13:19:56 +0530

    Added model for populating tableview

--- Get Fit/Models/ExerciseListDataModel.swift
+++ Get Fit/Models/ExerciseListDataModel.swift
@@ -5,4 +5,13 @@
 //  Created by Sandeep Sahani on 29/11/22.
 //
 
+// Model for exercise list view controller to populate ExerciseListVC
 import Foundation
+
+struct ExerciseListDataModel
+{
+     var exerciseGIF: [String]
+     var exerciseName: [String]
+    var targetMuscle: [String]
+     var exerciseEquipment: [String]
+}
