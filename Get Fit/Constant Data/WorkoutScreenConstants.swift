9d81ff68b6fc2369abfa87c9a3f73a03aeb9bae0
ngud-119 <passiondev7@gmail.com>
2022-12-02 17:18:46 +0530

    Added data to total exercise array

--- Get Fit/Constant Data/WorkoutScreenConstants.swift
+++ Get Fit/Constant Data/WorkoutScreenConstants.swift
@@ -7,17 +7,17 @@
 
 import UIKit
 
-// Struct tot store constants string used is workout screen
+/// Struct to store constants string used is workout screen
 struct WorkoutScreenConstants
 {
-    // Array for storing API endpoints body name with respective card image
+    /// Array for storing API endpoints body name with respective card image
     public static let bodyPart: [String] = ["cardio","shoulders","upper%20arms","lower%20arms","chest","back","upper%20legs","lower%20legs"]
-    // Array for storing the card images.
+    /// Array for storing the card images.
     public static let cardImages: [UIImage] = [UIImage(named: "Cardio")!,UIImage(named: "Shoulder")!,UIImage(named: "Upper arms.jpeg")!,UIImage(named: "Lower arms.jpg")!,UIImage(named: "Chest.jpg")!,UIImage(named: "Back.jpeg")!,UIImage(named: "Leg.jpeg")!,UIImage(named: "Lower legs.jpg")!]
     
-    // Array for stroing the workout name.
+    /// Array for stroing the workout name.
     public static let workoutName: [String] = ["Cardio","Shoulder Workout","Upper arms","Lower arms","Chest Workout","Back Workout","Upper legs","Lower legs"]
     
-    // Array for storing total workouts.
-    public static let totalWorkouts: [Int] = [6,7,5,12,22,33,8,9]
+    /// Array for storing total workouts.
+    public static let totalWorkouts: [Int] = [29,144,292,37,164,203,227,59]
 }
