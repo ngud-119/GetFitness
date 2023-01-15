91d6fcf72862f3798441f782fec32fe2451ea96d
ngud-119 <passiondev7@gmail.com>
2023-01-15 22:10:35 +0530

    Function call

--- Get Fit/Controllers/Diet Plan Screens/Compositional Layout/Diet Plan View Controller/CollectionView Cell/FoodNutientsCollectionViewCell.swift
+++ Get Fit/Controllers/Diet Plan Screens/Compositional Layout/Diet Plan View Controller/CollectionView Cell/FoodNutientsCollectionViewCell.swift
@@ -11,7 +11,7 @@ import UIKit
 class FoodNutientsCollectionViewCell: UICollectionViewCell
 {
     /// Reusable identifier for collection view cell.
-    static let identifier = "FoodNutientsCollectionViewCell"
+    static let identifier = "foodNutrientsCollectionViewCell"
     
     @IBOutlet weak var headingTitle: UILabel!
     
@@ -23,8 +23,9 @@ class FoodNutientsCollectionViewCell: UICollectionViewCell
     /// This method will populate collection view
     public func configure(title: String,quantity: String,unit: String)
     {
-        // Fatal Error: found nil while unwraping optionals
-        // self.headingTitle.text = title
+        // Fatal Error: found nil while unwraping optionals.
+        // Error was due to same name of collection view cell class and reusable identifier.
+        self.headingTitle.text = title
         self.quantityLabel.text = quantity
         self.unitLabel.text = unit
     }
