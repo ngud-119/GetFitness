c9dceb2ed1117083d57f259883f84f0ff0aafb7b
ngud-119 <passiondev7@gmail.com>
2023-01-15 22:08:55 +0530

    Added code for image in header view

--- Get Fit/Controllers/Diet Plan Screens/Compositional Layout/Food Nutrients View Controller/HeaderImageReusableView.swift
+++ Get Fit/Controllers/Diet Plan Screens/Compositional Layout/Food Nutrients View Controller/HeaderImageReusableView.swift
@@ -10,27 +10,33 @@ import UIKit
 /// Class for custom header view for FoodNutrientsViewController.
 class HeaderImageReusableView: UICollectionReusableView
 {
-
-    static let identifier = "HeaderImageReusableView"
     
-    let foodImageView: UIImageView = {
-        
+    static let identifier = "headerImageReusableView"
+   
+    let foodImageView = {
+
         let foodImage = UIImageView(image: UIImage(named: "Chocolate Oatmeal"))
         foodImage.contentMode = .scaleAspectFill
         foodImage.layer.opacity = 0.60
         return foodImage
-        
+
     }()
     
+
     override init(frame: CGRect)
     {
         super.init(frame: frame)
         addSubview(foodImageView)
         foodImageView.fillSuperview()
-        
     }
     
-   
+    public func configureHeader(imageName: String)
+    {
+        if let unwrapedImage = UIImage(named: imageName)
+        {
+            foodImageView.image = unwrapedImage
+        }
+    }
     required init?(coder: NSCoder)
     {
         fatalError("init(coder:) has not been implemented")
