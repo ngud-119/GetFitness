8b065e2ce44c09cfa7f8a3ec69f86490d32c28b5
ngud-119 <passiondev7@gmail.com>
2023-01-23 23:22:20 +0530

    Creating layout with UIScrollView

--- Get Fit/Controllers/Diet Plan Screens/NutrientsViewController.swift
+++ Get Fit/Controllers/Diet Plan Screens/NutrientsViewController.swift
@@ -11,72 +11,78 @@ class NutrientsViewController: UIViewController
 {
 
     @IBOutlet weak var foodImageView: UIView!
-    @IBOutlet weak var foodName: UILabel!
-    @IBOutlet weak var foodQuantitiy: UILabel!
-    @IBOutlet weak var prepTime: UILabel!
-    @IBOutlet weak var carbs: UILabel!
-    @IBOutlet weak var fat: UILabel!
-    @IBOutlet weak var protein: UILabel!
-    @IBOutlet weak var ingredientsCollectionView: UICollectionView!
-    @IBOutlet weak var preparationStepsTableView: UITableView!
+//    @IBOutlet weak var foodName: UILabel!
+//    @IBOutlet weak var foodQuantitiy: UILabel!
+//    @IBOutlet weak var prepTime: UILabel!
+//    @IBOutlet weak var carbs: UILabel!
+//    @IBOutlet weak var fat: UILabel!
+//    @IBOutlet weak var protein: UILabel!
+//    @IBOutlet weak var ingredientsCollectionView: UICollectionView!
+//    @IBOutlet weak var preparationStepsTableView: UITableView!
+    @IBOutlet weak var foodView: UIView!
+    
+    @IBOutlet weak var ContaintsScrollView: UIScrollView!
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        foodImageView.roundCorners(corners: [.bottomRight,.bottomLeft], radius:40)
+        foodImageView.roundCorners(corners: [.bottomLeft,.bottomRight], radius:40)
+       
+        ContaintsScrollView.contentInsetAdjustmentBehavior = .never
+        foodImageView.layer.opacity = 0.60
         // Do any additional setup after loading the view.
     }
 }
 
 
-// MARK: Extention for ingredients list view with UICollectionView
-
-extension NutrientsViewController: UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout
-{
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
-    {
-        return DietPlan.Ingredients.foodName.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize
-    {
-        return CGSize(width: 96.0, height: 96.0)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
-    {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ingredientsCollectionViewCell", for: indexPath) as! FoodItemListCollectionViewCell
-        
-        cell.configure(foodName: DietPlan.Ingredients.foodName[indexPath.row], foodImage: DietPlan.Ingredients.foodImage[indexPath.row], foodQuantity: DietPlan.Ingredients.foodQuantity[indexPath.row])
-        
-        return cell
-    }
-
-}
-
-// MARK: Extention for preparation procedure using UITableView
-
-extension NutrientsViewController: UITableViewDelegate,UITableViewDataSource
-{
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
-    {
-        return DietPlan.PreparationProcedure.steps.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
-    {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "preparationTableViewCell", for: indexPath) as! PreparationTableViewCell
-        let steps = indexPath.row
-        cell.configureList(countNumber: "\(steps+1)", procedureText: DietPlan.PreparationProcedure.steps[indexPath.row])
-        return cell
-    }
-    
-    // To dynamically set the height of row according to content lenght of label
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat
-    {
-        return UITableView.automaticDimension
-    }
-
-}
-
-
+//// MARK: Extention for ingredients list view with UICollectionView
+//
+//extension NutrientsViewController: UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout
+//{
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
+//    {
+//        return DietPlan.Ingredients.foodName.count
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize
+//    {
+//        return CGSize(width: 96.0, height: 96.0)
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
+//    {
+//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ingredientsCollectionViewCell", for: indexPath) as! FoodItemListCollectionViewCell
+//
+//        cell.configure(foodName: DietPlan.Ingredients.foodName[indexPath.row], foodImage: DietPlan.Ingredients.foodImage[indexPath.row], foodQuantity: DietPlan.Ingredients.foodQuantity[indexPath.row])
+//
+//        return cell
+//    }
+//
+//}
+//
+//// MARK: Extention for preparation procedure using UITableView
+//
+//extension NutrientsViewController: UITableViewDelegate,UITableViewDataSource
+//{
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+//    {
+//        return DietPlan.PreparationProcedure.steps.count
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+//    {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "preparationTableViewCell", for: indexPath) as! PreparationTableViewCell
+//        let steps = indexPath.row
+//        cell.configureList(countNumber: "\(steps+1)", procedureText: DietPlan.PreparationProcedure.steps[indexPath.row])
+//        return cell
+//    }
+//
+//    // To dynamically set the height of row according to content lenght of label
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat
+//    {
+//        return UITableView.automaticDimension
+//    }
+//
+//}
+//
+//
