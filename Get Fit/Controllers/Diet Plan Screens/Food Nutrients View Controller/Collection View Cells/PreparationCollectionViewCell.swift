1b534124e89edeedc52eb6186202e25df18f0a40
ngud-119 <passiondev7@gmail.com>
2023-01-30 23:26:38 +0530

    Added code for preparation collection view cell

--- Get Fit/Controllers/Diet Plan Screens/Food Nutrients View Controller/Collection View Cells/PreparationCollectionViewCell.swift
+++ Get Fit/Controllers/Diet Plan Screens/Food Nutrients View Controller/Collection View Cells/PreparationCollectionViewCell.swift
@@ -6,24 +6,25 @@
 //
 import UIKit
 
-// Class for preparation table view cell.
-//class PreparationTableViewCell: UITableViewCell
-//{
-//
-//    @IBOutlet weak var stepsCountView: UIView!
-//    @IBOutlet weak var countNumber: UILabel!
-//    @IBOutlet weak var ProcedureText: UILabel!
-//
-//    override func awakeFromNib()
-//    {
-//        super.awakeFromNib()
-//        // Initialization code
-//        stepsCountView.makeViewCircular()
-//    }
-//
-//    public func configureList(countNumber: String, procedureText: String)
-//    {
-//        self.countNumber.text = countNumber
-//        self.ProcedureText.text = procedureText
-//    }
-//}
+/// Class for preparation collection view cell.
+final class PreparationCollectionViewCell: UICollectionViewCell
+{
+
+    static let identifier = "preparationCollectionViewCell"
+    @IBOutlet weak var stepsCountView: UIView!
+    @IBOutlet weak var countNumber: UILabel!
+    @IBOutlet weak var ProcedureText: UILabel!
+
+    override func awakeFromNib()
+    {
+        super.awakeFromNib()
+        // Initialization code
+        stepsCountView.makeViewCircular()
+    }
+
+    public func configureList(countNumber: String, procedureText: String)
+    {
+        self.countNumber.text = countNumber
+        self.ProcedureText.text = procedureText
+    }
+}
