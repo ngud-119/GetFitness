6df133370af59fb3e71afa1c33a95a197559cbac
ngud-119 <passiondev7@gmail.com>
2023-02-01 23:25:44 +0530

    Changes commits

--- Get Fit/Models/Diet Plan View Controller/FoodCardModel.swift
+++ Get Fit/Models/Diet Plan View Controller/FoodCardModel.swift
@@ -7,11 +7,14 @@
 
 import UIKit
 
+/// Struct for storing data for a recipe.
 struct FoodCardModel
 {
+    /// For Diet Plan View Controller's (Food Card Collection View).
     var foodCategory: String
     var cardImage: [String]
     var foodName: [String]
     var foodQuantity: [Double]
     var foodCalorie: [Double]
+
 }
